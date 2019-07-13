1225ce311152c3815ad13f4fe7b38086f88ff1db
khan.mza@gmail.com <khan.mza@gmail.com>
2019-07-13 00:45:32 -0400

    Finally all good , now we get the data read in as stdin in pandas df and then trqnspose it feding it to the plotly express and thne implemneting it in A fash front end

--- pythongeneraterandomInputsMachine.py
+++ pythongeneraterandomInputsMachine.py
@@ -11,6 +11,9 @@ import datetime
 from random import randrange
 
 NUMITEMS=100
+CONTROL_FLIP=0
+
+
 def random_date(start,l):
    current = start
    while l >= 0:
@@ -33,13 +36,16 @@ print (",".join(timeslist))
 """
 
 
+
     
-def generateIncDecMixListRandomList(dLO,dHI,dNumofItems,sDirection):
+
+def generateIncDecMixListRandomList(dLO,dHI,dNumofItems,sDirection,CONTROL_FLIP):
     
     a=[]
     m=[]
     machine_list=["bublues","buParvatti","buCoumbRiyadh","buRenaisanceVector","buHyperion"]    
     randtimeslist=[]
+  
 
     for j in range(dNumofItems):
         randtime = datetime.datetime(2013, 9, 20,random.randint(1,23),random.randint(1,59))
@@ -48,11 +54,14 @@ def generateIncDecMixListRandomList(dLO,dHI,dNumofItems,sDirection):
         randtimeslist.append(randtime.strftime("%H%M"))
 
     #print (m)
-    randtimeslist.insert(0, "Times")
-    print (",".join(randtimeslist))
-        
-    m.insert(0,"MachinesNames")
-    print (str(m)[1:-1])
+    if (CONTROL_FLIP==0): 
+    	randtimeslist.insert(0, "Times")
+    	print (",".join(randtimeslist))
+    	m.insert(0,"MachinesNames")
+    	print (str(m)[1:-1])
+    	
+
+
     if sDirection == "Inc":
         a.sort()
         print (str(a)[1:-1])
@@ -66,15 +75,15 @@ def generateIncDecMixListRandomList(dLO,dHI,dNumofItems,sDirection):
     
     
 #print (random.randrange(10,20,3))
-print ("IncCPU|",end=" ") 
-generateIncDecMixListRandomList(10,90,NUMITEMS,"Inc")
-print ("IncMEM|",end=" ") 
-generateIncDecMixListRandomList(10,90,NUMITEMS,"Inc")
-print ("DecCPU|",end=" ") 
-generateIncDecMixListRandomList(10,90,NUMITEMS,"Dec")
-print ("DecMEM|",end=" ") 
-generateIncDecMixListRandomList(10,90,NUMITEMS,"Dec")
-print ("MixCPU|",end=" ") 
-generateIncDecMixListRandomList(10,90,NUMITEMS,"Mix")
-print ("MixMEM|",end=" ") 
-generateIncDecMixListRandomList(10,90,NUMITEMS,"Mix")
+print ("1.IncCPU|",end=" ") 
+generateIncDecMixListRandomList(10,90,NUMITEMS,"Inc",0)
+print ("1.IncMEM|",end=" ") 
+generateIncDecMixListRandomList(10,90,NUMITEMS,"Inc",1)
+print ("2.DecCPU|",end=" ") 
+generateIncDecMixListRandomList(10,90,NUMITEMS,"Dec",1)
+print ("2.DecMEM|",end=" ") 
+generateIncDecMixListRandomList(10,90,NUMITEMS,"Dec",1)
+print ("3.MixCPU|",end=" ") 
+generateIncDecMixListRandomList(10,90,NUMITEMS,"Mix",1)
+print ("3.MixMEM|",end=" ") 
+generateIncDecMixListRandomList(10,90,NUMITEMS,"Mix",1)
