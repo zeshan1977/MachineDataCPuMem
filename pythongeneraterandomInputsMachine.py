bb5c4f4d9c5513ef3ba13547c5dae8e8004564f3
khan.mza@gmail.com <khan.mza@gmail.com>
2019-07-12 23:31:37 -0400

    generating rand data includbng time,machinename,CPUand MEM

--- pythongeneraterandomInputsMachine.py
+++ pythongeneraterandomInputsMachine.py
@@ -7,7 +7,30 @@ Created on Wed Jul 10 20:56:05 2019
 """
 
 import random
+import datetime
+from random import randrange
 
+NUMITEMS=100
+def random_date(start,l):
+   current = start
+   while l >= 0:
+      curr = current + datetime.timedelta(minutes=randrange(60))
+      yield curr
+      l-=1
+
+startDate = datetime.datetime(2013, 9, 20,random.randint(1,23),00)
+timeslist=[]
+
+"""
+for x in random_date(startDate,10):
+    y = datetime.datetime(2013, 9, 20,random.randint(1,23),random.randint(1,59))
+    #print  (x.strftime("%H:%M") )
+    #print  (y.strftime("%H:%M") )
+    timeslist.append( y.strftime("%H:%M"))
+
+print (",".join(timeslist))
+
+"""
 
 
     
@@ -16,11 +39,19 @@ def generateIncDecMixListRandomList(dLO,dHI,dNumofItems,sDirection):
     a=[]
     m=[]
     machine_list=["bublues","buParvatti","buCoumbRiyadh","buRenaisanceVector","buHyperion"]    
+    randtimeslist=[]
+
     for j in range(dNumofItems):
+        randtime = datetime.datetime(2013, 9, 20,random.randint(1,23),random.randint(1,59))
         a.append(random.randint(dLO,dHI))
         m.append(random.choice(machine_list))
+        randtimeslist.append(randtime.strftime("%H%M"))
 
     #print (m)
+    randtimeslist.insert(0, "Times")
+    print (",".join(randtimeslist))
+        
+    m.insert(0,"MachinesNames")
     print (str(m)[1:-1])
     if sDirection == "Inc":
         a.sort()
@@ -35,9 +66,15 @@ def generateIncDecMixListRandomList(dLO,dHI,dNumofItems,sDirection):
     
     
 #print (random.randrange(10,20,3))
-print ("Inc|",end=" ") 
-generateIncDecMixListRandomList(10,90,30,"Inc")
-print ("Dec|",end=" ") 
-generateIncDecMixListRandomList(10,90,30,"Dec")
-print ("Mix|",end=" ") 
-generateIncDecMixListRandomList(10,90,30,"Mix")
+print ("IncCPU|",end=" ") 
+generateIncDecMixListRandomList(10,90,NUMITEMS,"Inc")
+print ("IncMEM|",end=" ") 
+generateIncDecMixListRandomList(10,90,NUMITEMS,"Inc")
+print ("DecCPU|",end=" ") 
+generateIncDecMixListRandomList(10,90,NUMITEMS,"Dec")
+print ("DecMEM|",end=" ") 
+generateIncDecMixListRandomList(10,90,NUMITEMS,"Dec")
+print ("MixCPU|",end=" ") 
+generateIncDecMixListRandomList(10,90,NUMITEMS,"Mix")
+print ("MixMEM|",end=" ") 
+generateIncDecMixListRandomList(10,90,NUMITEMS,"Mix")
